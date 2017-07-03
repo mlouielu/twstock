05240d24f6184b015422e2e1996fb90d7f6d7654
Louie Lu <git@louie.lu>
2017-07-03 14:06:41 +0800

    Fix best four point cli

--- twstock/cli/best_four_point.py
+++ twstock/cli/best_four_point.py
@@ -1,19 +1,17 @@
 import twstock
 
 
-def main(argv):
+def run(argv):
     print('四大買賣點判斷 Best Four Point')
     print('------------------------------')
-    if len(argv) > 1:
-        sids = argv[1:]
-        for sid in sids:
-            bfp = twstock.BestFourPoint(twstock.Stock(sid))
-            bfp = bfp.best_four_point()
-            print('%s: ' % (sid), end='')
-            if bfp:
-                if bfp[0]:
-                    print('Buy  ', bfp[1])
-                else:
-                    print('Sell ', bfp[1])
+    for sid in argv:
+        bfp = twstock.BestFourPoint(twstock.Stock(sid))
+        bfp = bfp.best_four_point()
+        print('%s: ' % (sid), end='')
+        if bfp:
+            if bfp[0]:
+                print('Buy  ', bfp[1])
             else:
-                print("Don't touch")
+                print('Sell ', bfp[1])
+        else:
+            print("Don't touch")
