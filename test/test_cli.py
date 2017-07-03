b5ff42caec89a7cf2303f9816ee0d70c713cc24e
Louie Lu <git@louie.lu>
2017-07-03 14:16:51 +0800

    Add cli test cases

---
+++ test/test_cli.py
@@ -0,0 +1,13 @@
+import unittest
+from twstock import cli
+
+
+class CLIFunctionTest(unittest.TestCase):
+    def setUp(self):
+        self.stocks = ['2330', '6223']
+
+    def test_best_four_point(self):
+        cli.best_four_point.run(self.stocks)
+
+    def test_stock(self):
+        cli.stock.run(self.stocks)
