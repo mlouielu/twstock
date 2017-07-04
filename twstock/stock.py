268b3ad5a845c4ccee937af8f9aea1937b81642b
Louie Lu <git@louie.lu>
2017-07-04 08:44:51 +0800

    Fix fetch_31 not fetching current month

--- twstock/stock.py
+++ twstock/stock.py
@@ -113,7 +113,7 @@ class Stock(analytics.Analytics):
 
     def _month_year_iter(self, start_month, start_year, end_month, end_year):
         ym_start = 12 * start_year + start_month - 1
-        ym_end = 12 * end_year + end_month - 1
+        ym_end = 12 * end_year + end_month
         for ym in range(ym_start, ym_end):
             y, m = divmod(ym, 12)
             yield y, m + 1
