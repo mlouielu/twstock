559203bfb0a7d4d58577e0e92ba3abbd802e5dca
Louie Lu <git@louie.lu>
2017-07-24 20:28:13 +0800

    Move cli

--- twstock/__main__.py
+++ twstock/__main__.py
@@ -1,20 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import argparse
-from . import cli
+from twstock import cli
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('-b', '--bfp', nargs='+')
-    parser.add_argument('-s', '--stock', nargs='+')
-    parser.add_argument('-r', '--realtime', nargs='+')
-    args = parser.parse_args()
-
-    if args.bfp:
-        cli.best_four_point.run(args.bfp)
-    elif args.stock:
-        cli.stock.run(args.stock)
-    elif args.realtime:
-        cli.realtime.run(args.realtime)
+    cli.run()
--- twstock/cli/__init__.py
+++ twstock/cli/__init__.py
@@ -1,3 +1,22 @@
-from . import best_four_point
-from . import stock
-from . import realtime
+# -*- coding: utf-8 -*-
+
+import argparse
+from twstock.cli import best_four_point
+from twstock.cli import stock
+from twstock.cli import realtime
+
+
+def run():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-b', '--bfp', nargs='+')
+    parser.add_argument('-s', '--stock', nargs='+')
+    parser.add_argument('-r', '--realtime', nargs='+')
+    args = parser.parse_args()
+
+    if args.bfp:
+        cli.best_four_point.run(args.bfp)
+    elif args.stock:
+        cli.stock.run(args.stock)
+    elif args.realtime:
+        cli.realtime.run(args.realtime)
