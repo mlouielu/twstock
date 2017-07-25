27d551dd52149781f9885cf28cf980fd00d10047
Louie Lu <git@louie.lu>
2017-07-25 20:01:34 +0800

    Fix bfp cli output

--- twstock/cli/best_four_point.py
+++ twstock/cli/best_four_point.py
@@ -10,15 +10,15 @@ stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8', errors='replace')
 
 def run(argv):
     print('四大買賣點判斷 Best Four Point', file=stdout)
-    print('------------------------------')
+    print('------------------------------', file=stdout)
     for sid in argv:
         bfp = twstock.BestFourPoint(twstock.Stock(sid))
         bfp = bfp.best_four_point()
-        print('%s: ' % (sid), end='')
+        print('%s: ' % (sid), end='', file=stdout)
         if bfp:
             if bfp[0]:
                 print('Buy  ', bfp[1], file=stdout)
             else:
                 print('Sell ', bfp[1], file=stdout)
         else:
-            print("Don't touch")
+            print("Don't touch", file=stdout)
