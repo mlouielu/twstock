32187aab2d2cd1a2a6aefd227e966511b1a8b81d
Louie Lu <git@louie.lu>
2017-07-25 09:58:46 +0800

    Fix encoding to cli files on Windows cmd.exe

--- twstock/cli/best_four_point.py
+++ twstock/cli/best_four_point.py
@@ -1,8 +1,15 @@
+# -*- coding: utf-8 -*-
+
+import io
+import sys
 import twstock
+
+# XXX: Repalce sys.stdout prevent Windows UnicodeEncodeError on cmd.exe
+stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8', errors='replace')
 
 
 def run(argv):
-    print('四大買賣點判斷 Best Four Point')
+    print('四大買賣點判斷 Best Four Point', file=stdout)
     print('------------------------------')
     for sid in argv:
         bfp = twstock.BestFourPoint(twstock.Stock(sid))
@@ -10,8 +17,8 @@ def run(argv):
         print('%s: ' % (sid), end='')
         if bfp:
             if bfp[0]:
-                print('Buy  ', bfp[1])
+                print('Buy  ', bfp[1], file=stdout)
             else:
-                print('Sell ', bfp[1])
+                print('Sell ', bfp[1], file=stdout)
         else:
             print("Don't touch")
