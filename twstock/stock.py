bacd1e2741dcf6ebb077985e98ea6c060d824c54
陳泰澄 <TCCinTaiwan@users.noreply.github.com>
2017-10-17 06:52:20 +0800

    Revert "Stochastic oscillator"

--- twstock/stock.py
+++ twstock/stock.py
@@ -4,7 +4,6 @@ import datetime
 import json
 import urllib.parse
 from collections import namedtuple
-import sys
 
 import requests
 
@@ -46,10 +45,9 @@ class TWSEFetcher(BaseFetcher):
     def fetch(self, year: int, month: int, sid: str, retry=5):
         params = {'date': '%d%02d01' % (year, month), 'stockNo': sid}
         r = requests.get(self.REPORT_URL, params=params)
-        json_decode_error = ValueError if sys.version_info < (3, 5) else json.decoder.JSONDecodeError
         try:
             data = r.json()
-        except json_decode_error:
+        except json.decoder.JSONDecodeError:
             if retry:
                 return self.fetch(year, month, sid, retry - 1)
             data = {'stat': '', 'data': []}
