aa0ee5a1c8c8d12f0e7484fb267cc46d59ac2772
Louie Lu <git@louie.lu>
2019-05-13 19:32:52 +0800

    Add round robin proxies provider

--- twstock/proxy.py
+++ twstock/proxy.py
@@ -1,5 +1,7 @@
 import abc
 
+from itertools import cycle
+
 
 class ProxyProvider(abc.ABC):
     @abc.abstractmethod
@@ -18,6 +20,27 @@ class SingleProxyProvider(ProxyProvider):
 
     def get_proxy(self):
         return self._proxy
+
+
+class RoundRobinProxiesProvider(ProxyProvider):
+    def __init__(self, proxies: list):
+        self._proxies = proxies
+        self._proxies_cycle = cycle(proxies)
+
+    @property
+    def proxies(self):
+        return self._proxies
+
+    @proxies.setter
+    def proxies(self, proxies: list):
+        if not isinstance(proxies, list):
+            raise ValueError('Proxies only accept list')
+
+        self._proxies = proxies
+        self._proxies_cycle = cycle(proxies)    
+
+    def get_proxy(self):
+        return next(self._proxies_cycle)
 
 
 _provider_instance = NoProxyProvier()
